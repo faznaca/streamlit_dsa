9718f4bc1157689ece103919f360963ac9334924
Fazna C A <156344707+faznaca@users.noreply.github.com>
2024-04-20 06:42:30 +0000

    1

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pickle
+
+st.header("Predict weight from height")
+
+st.write("Demo of linear regression")
+
+
+height = st.number_input(label = "Enter height in Inches", min_value = 35.0, max_value = 120.0, value = 50.00, step = 1.0)
+
+submitted = st.button('Submit')
+
+if submitted:
+    pickle_model = pickle.load(open('model.pkl', 'rb'))
+    weight = pickle_model.predict([[height]])
+    st.write(f"Expected weight value is **{weight[0]}** pounds")
